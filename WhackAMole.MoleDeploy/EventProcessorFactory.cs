1c98700abe48c651a3e201d81d7e5cc337b15bb5
Joey Lorich <joseph.lorich@microsoft.com>
2018-05-23 14:13:54 -0600

    eh

---
+++ WhackAMole.MoleDeploy/EventProcessorFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhackAMole.MoleDeploy
+{
+    public class EventProcessorFactory : IEventProcessorFactory
+    {
+        private readonly Func<string, Task> _Handler;
+
+        public EventProcessorFactory(Func<string, Task> handler)
+        {
+            _Handler = handler;
+        }
+
+        public IEventProcessor CreateEventProcessor(PartitionContext context)
+        {
+            var processor = new EventProcessor(_Handler);
+            return processor;
+        }
+    }
+}
