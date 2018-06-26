8ba0cec4c3816b6c29b4725c0239e80eb9fbfe26
Joey Lorich <joseph.lorich@microsoft.com>
2018-06-26 16:31:29 -0600

    Update enum contract json

--- MoleDeploy.Contracts/VstsBuildStateChangeNotification.cs
+++ MoleDeploy.Contracts/VstsBuildStateChangeNotification.cs
@@ -1,7 +1,11 @@
-﻿namespace MoleDeploy.Contracts
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MoleDeploy.Contracts
 {
     public class VstsBuildStateChangeNotification
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public VstsBuildState State { get; set; }
     }
 }
